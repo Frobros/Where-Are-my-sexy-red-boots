a53ea89a0673b2f2ef3f5dad95daf05a156a5c69
locojoetive <johannes.mic@gmail.com>
2021-04-25 17:18:52 +0200

    implements textbox manager

--- Assets/Actor.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class Actor : MonoBehaviour
-{
-	public abstract void Move(Vector2 vector2);
-	public abstract void Zoom(int zoomFactor);
-	public abstract void Attack(Vector2 vector2);
-}
---
+++ Assets/_prefabs/Camera/FollowTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target2D;
+    public Transform alternativeTarget;
+    public float smoothDamp;
+
+    void LateUpdate()
+    {
+        Transform target = alternativeTarget ? alternativeTarget : target2D;
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothDamp);
+    }
+}
